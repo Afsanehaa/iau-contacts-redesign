a2eafe848dfad14b0942ae9239dc8ac5238a7480
Afsanehaa <afsanehabdid@gmail.com>
2026-06-16 14:58:09 -0700

    feat: Creating data

---
+++ src/data/provinces.js
@@ -0,0 +1,303 @@
+export const provinces = [
+  {
+    id: 1,
+    name: "آذربایجان شرقی",
+    slug: "east-azerbaijan",
+    cities: [
+      {
+        id: 101,
+        name: "ورزقان",
+        support: "کامران کوزه گر",
+        phone: "09121234567",
+        website: "varzaghan.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2varzeghan"
+      },{
+        id: 102,
+        name: "هریس",
+        support: "هادی شهامفر",
+        phone: "09125834567",
+        website: "heris.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2heris"
+      },{
+        id: 103,
+        name: "هشترود",
+        support: "رضا شعاری" ,
+        phone: "09121898567",
+        website: "hashtrod.iau.ir",
+        vadana: "vadana01.ec.aiu.ir",
+        messenger: "http://vadana1hashtrod"
+      }
+    ]
+  },{
+    id: 2,
+    name: " اردبیل",
+    slug: "ardebil",
+    cities: [
+      {
+        id: 104,
+        name: "واحد اردبیل",
+        support: " ایرج قربانی",
+        phone: "09121225867",
+        website: "ardebil.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2ardebil"
+      },{
+        id: 105,
+        name: "واحد خلخال",
+        support: " عارف ترابی",
+        phone: "09121234512",
+        website: "khalkhal.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2khalkhal"
+      },{
+        id: 106,
+        name: "واحد گرمی",
+        support: " وحید رشیدی",
+        phone: "09121234789",
+        website: "germi.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2germi"
+      }
+    ]
+  },{
+    id: 3,
+    name: " البرز",
+    slug: "alborz",
+    cities: [
+      {
+        id: 107,
+        name: "واحد کرج",
+        support: " شهرزاد ستوده",
+        phone: "09151234567",
+        website: "karaj.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2karaj"
+      },{
+        id: 108,
+        name: "مرکز نظرآباد",
+        support: " شکوفه رضایی",
+        phone: "09143021064",
+        website: "nazarabad.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2nazarabad"
+      },{
+        id: 109,
+        name: "واحد هشتگرد",
+        support: " یگانه بیگی",
+        phone: "09131234567",
+        website: "hashtgard.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2hashtgard"
+      }
+    ]
+  },{
+    id: 4,
+    name: "ایلام",
+    slug: "ilam",
+    cities: [
+      {
+        id: 110,
+        name: "ایلام",
+        support: " فریده محمدی",
+        phone: "09125021064",
+        website: "ilam.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2ilam"
+      }
+    ]
+  },{
+    id: 5,
+    name: " بوشهر",
+    slug: "boshehr",
+    cities: [
+      {
+        id: 111,
+        name: "واحد بوشهر",
+        support: " ارسلان فلاحتکار",
+        phone: "09136021064",
+        website: "boshehr.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2boshehr"
+      },{
+        id: 112,
+        name: "مرکز کنگان",
+        support: " عبدالمجید سبوطی",
+        phone: "09189021064",
+        website: "kangan.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2kangan"
+      },{
+        id: 113,
+        name: "واحد لوار",
+        support: " محمد محمدی تبار",
+        phone: "09173021064",
+        website: "lavar.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2lavar"
+      }
+    ]
+  },{
+    id: 6,
+    name: " تهران",
+    slug: "tehran",
+    cities: [
+      {
+        id: 114,
+        name: "تهران جنوب",
+        support: " نرگس توکلی",
+        phone: "09136021064",
+        website: "tehranjonob.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2tehranjonob"
+      },{
+        id: 115,
+        name: " دماوند",
+        support: " امیر حاجی امینی",
+        phone: "09143369064",
+        website: "damavand.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2damavand"
+      },{
+        id: 116,
+        name: "فیروزکوه",
+        support: " علی احمدپور",
+        phone: "09112521064",
+        website: "firozkoh.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2firozkoh"
+      }
+    ]
+  },{
+    id: 7,
+    name: "چهارمحال و بختیاری",
+    slug: "chaharmahal and bakhtiari",
+    cities: [
+      {
+        id: 117,
+        name: "واحد شهرکرد",
+        support: "کامران کوزه گر",
+        phone: "09145421064",
+        website: "shahrkord.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2shahrkord"
+      },{
+        id: 118,
+        name: "واحد بروجن",
+        support: "کامران کوزه گر",
+        phone: "09163021064",
+        website: "borojen.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2borojen"
+      },{
+        id: 119,
+        name: "مرکز فارسان",
+        support: "کامران کوزه گر",
+        phone: "09193021064",
+        website: "farsiyan.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2farsiyan"
+      }
+    ]
+  },{
+    id: 8,
+    name: "خراسان رضوی",
+    slug: "khorasan razavi",
+    cities: [
+      {
+        id: 120,
+        name: "مشهد",
+        support: "کامران کوزه گر",
+        phone: "09193021164",
+        website: "mashhad.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://vadana2mashhad"
+      },
+      {
+        id: 121,
+        name: "گناباد",
+        support: "احمدی",
+        phone: "04131111111",
+        website: "gonabad.iau.ir",
+        vadana: "vadana01.ec.aiu.ir",
+        messenger: "http://t.me/gonabad"
+      },
+      {
+        id: 122,
+        name: "بردسکن",
+        support: "احمدی",
+        phone: "02136511111",
+        website: "bardskan.iau.ir",
+        vadana: "vadana01.ec.aiu.ir",
+        messenger: "http://t.me/bardskan"
+      }
+    ]
+  },{
+    id: 9,
+    name: "خراسان شمالی",
+    slug: "north-khorasan",
+    cities: [
+      {
+        id: 123,
+        name: "واحد بجنورد",
+        support: "کامران کوزه گر",
+        phone: "09123921064",
+        website: "bojnord.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://bojnord"
+      },
+      {
+        id: 124,
+        name: "واحد شیروان",
+        support: "احمدی",
+        phone: "02131111156",
+        website: "shirvan.iau.ir",
+        vadana: "vadana01.ec.aiu.ir",
+        messenger: "http://t.me/shirvan"
+      },
+      {
+        id: 125,
+        name: "واحد اسفراین",
+        support: "احمدی",
+        phone: "04131145611",
+        website: "esfarayen.iau.ir",
+        vadana: "vadana01.ec.aiu.ir",
+        messenger: "http://t.me/esfarayen"
+      }
+    ]
+  },{
+    id: 10,
+    name: "زنجان",
+    slug: "zanjan",
+    cities: [
+      {
+        id: 126,
+        name: "واحد خدابنده",
+        support: "کامران کوزه گر",
+        phone: "09143843064",
+        website: "khodabande.iau.ir",
+        vadana: "vadana02.ec.aiu.ir",
+        messenger: "http://khodabande"
+      },
+      {
+        id: 127,
+        name: "واحد هیدج",
+        support: "احمدی",
+        phone: "05131239111",
+        website: "hidaj.iau.ir",
+        vadana: "vadana01.ec.aiu.ir",
+        messenger: "http://t.me/hidaj"
+      },
+      {
+        id: 128,
+        name: "واحد ابهر",
+        support: "احمدی",
+        phone: "04131139711",
+        website: "abhar.iau.ir",
+        vadana: "vadana01.ec.aiu.ir",
+        messenger: "http://t.me/abhar"
+      }
+    ]
+  }
+];
